1a0c4c6f456335b368efa2c39c611ff058ab1402
Noel Ortiz <20175580@itla.edu.do>
2020-11-22 19:03:24 -0400

    banco de rostros y demas...

---
+++ clase1.py
@@ -0,0 +1,12 @@
+class my_class(object):
+    pass
+import cv2
+import os
+
+
+imagesPath = "C:/Users/noeld/source/repos/Vision-Computacional/Imagenes"
+imagesPathList = os.listdir(imagesPath)
+print('imagesPathList=',imagesPathList)
+
+
+
