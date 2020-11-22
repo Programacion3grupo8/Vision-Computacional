9e6f438d8e2f64d8eb99cdde92d958c1f0000e72
Noel Ortiz <20175580@itla.edu.do>
2020-11-22 01:47:58 -0400

    Primer almacenado de rostros

--- Main.py
+++ Main.py
@@ -1,31 +1,33 @@
 import cv2
 #capturamos video
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 #Ingresamos el algoritmo
-faceClassif = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
 while True:
-  ret,frame = cap.read()
+ # ret,frame = cap.read()
+  image = cv2.imread('oficina.png')
   imageAux = image.copy()
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
   faces = faceClassif.detectMultiScale(gray, 1.3, 5)
 
   count = 0
 
   for (x,y,w,h) in faces:
-    cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+    cv2.rectangle(image, (x,y),(x+w,y+h),(0,255,0),2)
     rostro = imageAux[y:y+h,x:x+w]
     rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite('rostro_{}.jpg'.format(count),rostro)
+    cv2.imwrite('rostro_{}.png'.format(count),rostro)
+    count = count + 1
 
-
-
-  cv2.imshow('frame',frame)
+    cv2.imshow('rostro', rostro)
+    cv2.imshow('image',image)
+   
   
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
-cap.release()
+#cap.release()
 cv2.destroyAllWindows()
 
 
